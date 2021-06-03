32c7f362e2d1e8065e634e44f1d39a74034aca28
monomalvado <teufelh@gmail.com>
2021-06-03 18:41:57 -0500

    Added a CMakeLists.txt file to enable compilation on CLion. Added a stub to allow compilation of the olcPixelGameEngine to a Library which can then be linked to.
    Right now the cmakelists is using a hardcoded reference to the location of the asio source code. This is to enable the Networking features of the olcPixelGameEngine.cpp

---
+++ olcPixelGameEngine.cpp
@@ -0,0 +1,23 @@
+#define OLC_PGE_APPLICATION
+#include "olcPixelGameEngine.h"
+
+#define OLC_PGEX_NETWORK
+#include "olcPGEX_Network.h"
+
+#define OLC_PGE_GRAPHICS2D
+#include "olcPGEX_Graphics2D.h"
+
+#define OLC_PGE_GRAPHICS3D
+#include "olcPGEX_Graphics3D.h"
+
+#define OLC_PGEX_POPUPMENU
+#include "olcPGEX_PopUpMenu.h"
+
+#define OLC_PGEX_RAYCASTWORLD
+#include "olcPGEX_RayCastWorld.h"
+
+#define OLC_PGEX_SOUND
+#include "olcPGEX_Sound.h"
+
+#define OLC_PGEX_TRANSFORMEDVIEW
+#include "olcPGEX_TransformedView.h"
